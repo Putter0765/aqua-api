1bec8b0db566828da9fe31b528d7b563525cc1d6
Putter0765 <88994768+Putter0765@users.noreply.github.com>
2025-03-17 19:32:04 +0700

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-python -m spacy download en_core_web_sm  # ติดตั้ง Spacy Model
+pip install -r requirements.txt  # ติดตั้ง dependencies
+python -m spacy download en_core_web_sm  # ดาวน์โหลดโมเดล Spacy
 python location-api.py &  # รัน location-api.py
 python name-api.py &  # รัน name-api.py
 python ocr-api.py  # รัน OCR API (ตัวสุดท้ายไม่ต้อง &)
